02a3e9c10789d83c17fc8101b75dacf9c45379ad
Quan Cao <caoanhquan_qn@yahoo.com>
2022-07-11 12:59:59 +0700

    add cart dropdown and contact page

---
+++ src/pages/contact/ContactPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContactPage.scss';
+import maintenanceLogo from '../../assets/img/maintenance.png';
+
+const ContactPage = () => {
+  return (
+    <div className="contact-page">
+      <img src={maintenanceLogo} />
+      <span>Something awesome is coming from Contact Page Project in August 2022</span>
+    </div>
+  );
+};
+
+export default ContactPage;
