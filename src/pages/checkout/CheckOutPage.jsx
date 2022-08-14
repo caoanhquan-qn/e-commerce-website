9bcfde064131333b358cdc5b1112c32c72f95a2c
Quan Cao <caoanhquan_qn@yahoo.com>
2022-08-15 00:11:12 +0700

    integrate stripe for payment

--- src/pages/checkout/CheckOutPage.jsx
+++ src/pages/checkout/CheckOutPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import CheckOutHeader from '../../components/checkout-header/CheckOutHeader';
-import CustomButton from '../../components/custom-button/CustomButton';
 import CheckOutItems from '../../components/checkout-items/CheckOutItems';
 import { selectCartItems } from '../../redux/selector';
 import './CheckOutPage.scss';
+import PaymentForm from '../../components/payment-form/PaymentForm';
 
 const CheckOutPage = () => {
   const cartItems = useSelector(selectCartItems);
@@ -18,10 +18,8 @@ const CheckOutPage = () => {
       <CheckOutItems checkOutItems={cartItems} />
       <div className="checkout-footer">
         <div className="total">TOTAL: ${total}</div>
-        <div>
-          <CustomButton buttonType="default">Pay Now</CustomButton>
-        </div>
       </div>
+      <PaymentForm />
     </div>
   );
 };
