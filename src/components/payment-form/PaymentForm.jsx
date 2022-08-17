2bc793259ef64fa4692fcbeb9422345f38867640
Quan Cao <caoanhquan_qn@yahoo.com>
2022-08-17 22:45:21 +0700

    fix serverless function for stripe payment

--- src/components/payment-form/PaymentForm.jsx
+++ src/components/payment-form/PaymentForm.jsx
@@ -3,23 +3,37 @@ import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import CustomButton from '../custom-button/CustomButton';
 import './PaymentForm.scss';
 
-const PaymentForm = () => {
+const PaymentForm = ({ amount }) => {
   const stripe = useStripe();
   const elements = useElements();
+  const amountInCents = amount * 100;
   const paymentHandler = async (e) => {
     e.preventDefault();
     if (!stripe || !elements) return;
-    const data = await fetch('/api/create-payment-intent.js', {
+    const response = await fetch('/api/create-payment-intent.js', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ amount: 10000 }),
-    })
-      .then((res) => res.json())
-      .catch((error) => console.log(error));
+      body: JSON.stringify({ amount: amountInCents }),
+    }).then((res) => res.json());
 
-    console.log(data);
+    const { client_secret } = response.paymentIntent;
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Quan',
+        },
+      },
+    });
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Successful Payment');
+      }
+    }
   };
   return (
     <form className="payment-form-container" onSubmit={paymentHandler}>
